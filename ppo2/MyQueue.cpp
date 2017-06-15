ef79ef9745048500a6caadfaf0f69a8409c9c40b
Egor Domnin <zombie9@inbox.ru>
2017-06-15 15:54:43 +0300

    Added deq\enq
    Deque test pass

--- ppo2/MyQueue.cpp
+++ ppo2/MyQueue.cpp
@@ -13,11 +13,31 @@ MyQueue::~MyQueue() {
 
 void MyQueue::enqueue(int value)
 {
+	if (inStack.size() + outStack.size() == MAX_ELEMENTS)
+		dequeue();
+
+	stackPush(inStack, value);
 }
 
 int MyQueue::dequeue()
 {
-	return 0;
+	assert(inStack.size() + outStack.size() != 0);
+
+	if (outStack.size() > 0) {
+		int t = outStack.top().value;
+		outStack.pop();
+		return t;
+	}
+	else {
+		while (!inStack.empty()) {
+			int t = inStack.top().value;
+			inStack.pop();
+			stackPush(outStack, t);
+		}
+		int t = outStack.top().value;
+		outStack.pop();
+		return t;
+	}
 }
 
 int MyQueue::minimum()
@@ -33,5 +53,24 @@ int MyQueue::maximum()
 vector<int> MyQueue::getQueue()
 {
 	return vector<int>();
+}
+
+void MyQueue::stackPush(stack<Element>& stack, int value)
+{
+	Element newElement(value, value, value);
+
+	if (stack.size() > 0) {
+		if (stack.top().max < value)
+			newElement.max = value;
+		else
+			newElement.max = stack.top().max;
+
+		if (stack.top().min > value)
+			newElement.min = value;
+		else
+			newElement.min = stack.top().min;
+	}
+
+	stack.push(newElement);
 }
 
--- ppo2/MyQueue.h
+++ ppo2/MyQueue.h
@@ -18,6 +18,30 @@ public:
 
 	virtual vector<int> getQueue();
 
+protected:
+	struct Element {
+		Element() :
+			max(0),
+			min(0),
+			value(0)
+		{
+		}
+
+		Element(int m_min, int m_max, int m_val) :
+			max(m_max),
+			min(m_min),
+			value(m_val)
+		{
+		}
+
+		int max;
+		int min;
+		int value;
+	};
+
+	void stackPush(stack<Element> &stack, int value);
+	stack<Element> inStack;
+	stack<Element> outStack;
 };
 
 
