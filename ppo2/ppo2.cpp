7a2441b560249eecb1bd9fb2c4f8463ef79b52eb
Egor Domnin <zombie9@inbox.ru>
2017-06-15 16:05:50 +0300

    Added console app

--- ppo2/ppo2.cpp
+++ ppo2/ppo2.cpp
@@ -8,7 +8,21 @@
 int main()
 {
 	Ique * qe = new MyQueue();
-
+	vector<int> queue;
+	rand();
+	int code, output, min = 0, max = 10;
+	printf("Input 0 to exit or any int to continue\n");
+	scanf_s("%d", &code);
+	while (code != 0) {
+		output = min + (rand() % (max - min + 1));
+		qe->enqueue(output);
+		queue = ((MyQueue*)qe)->getQueue();
+		printf("max:%d min:%d queue:", qe->maximum(), qe->minimum());
+		for (int i = 0; i < queue.size(); i++)
+			printf("%d ", queue[i]);
+		printf("\n");
+		scanf_s("%d", &code);
+	}
 	return 0;
 }
 
